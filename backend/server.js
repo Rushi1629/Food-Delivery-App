d9941b9fa2cd1b5221075c890de2ba91e6e8a8a5
rushikesh1629 <155706526+rushikesh1629@users.noreply.github.com>
2024-11-19 11:35:40 +0530

    render changes

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,14 @@ import userRouter from "./routes/userRoute.js";
 import "dotenv/config";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 
 // App Config
+
+const __filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
 
 const app = express();
 const port = process.env.port || 4000;
@@ -16,6 +22,9 @@ const port = process.env.port || 4000;
 
 app.use(express.json());
 app.use(cors());
+
+
+
 
 // DB Connection
 connectDB();
@@ -28,8 +37,18 @@ app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+app.use(express.static(path.join(__dirname, './frontend/dist')));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, './frontend/dist/index.html'))
+});
+
+app.use(express.static(path.join(__dirname, './admin/dist')));
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, './admin/dist/index.html'))
 });
 
 app.listen(port, () => {
