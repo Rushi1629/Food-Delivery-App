d889bfe3ed333b2cdef4c7348a3b79e6b2f501fd
rushikesh1629 <155706526+rushikesh1629@users.noreply.github.com>
2024-11-19 11:52:18 +0530

    hello

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -8,7 +8,8 @@ const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState([]);
 
-    const url = "http://localhost:4000";
+    // const url = "http://localhost:4000";
+    const url = "https://food-delivery-app-pltp.onrender.com";
 
     const [token, setToken] = useState("");
 
