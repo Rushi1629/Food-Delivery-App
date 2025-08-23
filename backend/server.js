a2cd44d7aefc53e20007211f9b300495c4d589ea
rushikesh1629 <rushikeshbhor1629@gmail.com>
2025-08-23 19:58:13 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 // Middleware
 
